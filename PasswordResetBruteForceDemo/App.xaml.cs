dec69ec96c855970f35d849047fa104a829cfe31
MAR7032 <muhammadabdulrehmaneducation@gmail.com>
2026-06-02 19:40:54 +0300

    Add project files.

---
+++ PasswordResetBruteForceDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PasswordResetBruteForceDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
