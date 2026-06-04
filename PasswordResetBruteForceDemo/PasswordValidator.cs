4bbcea22c47179d6f0485e6f21d1c2ea17a014a0
MAR7032 <muhammadabdulrehmaneducation@gmail.com>
2026-06-04 21:39:03 +0300

    Added password validator class

---
+++ PasswordResetBruteForceDemo/PasswordValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace PasswordResetBruteForceDemo
+{
+    public class PasswordValidator
+    {
+        private HashService hashService = new HashService();
+
+        public bool IsPasswordCorrect(string guessedPassword, string targetHash)
+        {
+            string guessedHash = hashService.ComputeSha256Hash(guessedPassword);
+
+            if (guessedHash == targetHash)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
