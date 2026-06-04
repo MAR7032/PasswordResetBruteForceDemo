03f373170917476b71002b00746cbcb4286cde97
MAR7032 <muhammadabdulrehmaneducation@gmail.com>
2026-06-04 21:12:36 +0300

    Added SHA256 hash service

---
+++ PasswordResetBruteForceDemo/HashService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordResetBruteForceDemo
+{
+    public class HashService
+    {
+        // Constant static salt required by the assignment.
+        private const string Salt = "StaticSalt2026";
+
+        public string ComputeSha256Hash(string password)
+        {
+            // Password and salt are joined before hashing.
+            string passwordWithSalt = password + Salt;
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(passwordWithSalt);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                string hashText = "";
+
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    hashText = hashText + hashBytes[i].ToString("x2");
+                }
+
+                return hashText;
+            }
+        }
+    }
+}
