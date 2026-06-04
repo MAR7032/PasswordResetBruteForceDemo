16823002a022ed7c8e552993ccdbc9353da0bfdb
MAR7032 <muhammadabdulrehmaneducation@gmail.com>
2026-06-04 21:42:32 +0300

    Added brute force generator class

---
+++ PasswordResetBruteForceDemo/BruteForceGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace PasswordResetBruteForceDemo
+{
+    public class BruteForceGenerator
+    {
+        // Same character set as PasswordGenerator.
+        private const string Characters = "abc123";
+
+        public List<string> GenerateCombinations(int length)
+        {
+            List<string> results = new List<string>();
+
+            GenerateRecursive("", length, results);
+
+            return results;
+        }
+
+        private void GenerateRecursive(string currentText, int targetLength, List<string> results)
+        {
+            if (currentText.Length == targetLength)
+            {
+                results.Add(currentText);
+                return;
+            }
+
+            for (int i = 0; i < Characters.Length; i++)
+            {
+                GenerateRecursive(currentText + Characters[i], targetLength, results);
+            }
+        }
+    }
+}
