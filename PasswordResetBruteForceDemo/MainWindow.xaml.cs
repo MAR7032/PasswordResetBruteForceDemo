25c8e3439abf5b54bdf63b96a5d6e0312a65a5b4
MAR7032 <muhammadabdulrehmaneducation@gmail.com>
2026-06-04 21:34:22 +0300

    Connected password generation to GUI

--- PasswordResetBruteForceDemo/MainWindow.xaml.cs
+++ PasswordResetBruteForceDemo/MainWindow.xaml.cs
@@ -1,24 +1,29 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PasswordResetBruteForceDemo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private PasswordGenerator passwordGenerator = new PasswordGenerator();
+        private HashService hashService = new HashService();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GeneratePasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            string password = passwordGenerator.GeneratePassword();
+            string hash = hashService.ComputeSha256Hash(password);
+
+            GeneratedPasswordTextBox.Text = password;
+            HashTextBox.Text = hash;
+
+            FoundPasswordTextBox.Text = "";
+            ElapsedTimeTextBox.Text = "";
+            AttackProgressBar.Value = 0;
+            PerformanceLogTextBox.Text = "Password generated and hashed successfully.";
         }
     }
 }
